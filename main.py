3b6118dd579b8fb318b36f8eaa3a17e4e5946ce2
Aurelien BOUIN <a.bouin@gmail.com>
2019-09-05 17:50:46 +0200

    Adjust start screen and template to 1920x1080

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ def init_environment():
     """Function that initialize environment"""
     environment = {}
     environment["output_photos_folder"] = "output_photos"
-    environment["output_montages_photos_folder"] = "output_photos/images"
+    environment["output_montages_photos_folder"] = "output_photos/montages"
     environment["tmp_photo_print_path"] = "/tmp/tempprint.png"
     environment["template_path"] = "images/template.png"
     environment["last_taken_picture_path"] = None
@@ -96,19 +96,40 @@ def init_environment():
     environment["camera_parameters"]["rotation"] = 0
     environment["camera_parameters"]["hflip"] = False
     environment["camera_parameters"]["vflip"] = False
-    environment["camera_parameters"]["brightness"] = 50
+    #environment["camera_parameters"]["brightness"] = 50
     environment["camera_parameters"]["preview_alpha"] = 120
     environment["camera_parameters"]["preview_fullscreen"] = True
+    environment["camera_parameters"]["video_stabilization"] = True
     environment["camera_pointer"] = None
+
+    environment["picture_for_pasting_width"] = 550
+    environment["picture_for_pasting_height"] = 360
+
+    environment["montage_rebord"] = 30
 
     return environment
 
 ImageShowed = False
 Printing = False
-#IMAGE_WIDTH = 558
-#IMAGE_HEIGHT = 374
-IMAGE_WIDTH = 550
-IMAGE_HEIGHT = 360
+
+def compute_picture_size_and_position(environment):
+    """ Compute picture size and position to fit the screen """
+    environment["picture_for_pasting_width"] = (environment["screen_w"] - (3 * environment["montage_rebord"]) ) /2
+    environment["picture_for_pasting_height"] = (environment["screen_h"] - (3 * environment["montage_rebord"]) ) /2
+
+    delta_x = environment["picture_for_pasting_width"] + (2 * environment["montage_rebord"])
+    delta_y = environment["montage_rebord"]
+    environment["picture_for_pasting_pos1"] = (delta_x, delta_y)
+    delta_y = environment["picture_for_pasting_height"] + (2 * environment["montage_rebord"])
+    environment["picture_for_pasting_pos2"] = (delta_x, delta_y)
+    delta_x = environment["montage_rebord"]
+    environment["picture_for_pasting_pos3"] = (delta_x, delta_y)
+
+    lg.info("picture_for_pasting_width:"+str(environment["picture_for_pasting_width"]))
+    lg.info("picture_for_pasting_height:"+str(environment["picture_for_pasting_height"]))
+    lg.info("picture_for_pasting_pos1:"+str(environment["picture_for_pasting_pos1"]))
+    lg.info("picture_for_pasting_pos2:"+str(environment["picture_for_pasting_pos2"]))
+    lg.info("picture_for_pasting_pos3:"+str(environment["picture_for_pasting_pos3"]))
 
 def setup_pygame(environment):
     """ Setup pygame environment """
@@ -134,9 +155,6 @@ def setup_pygame(environment):
     environment["replay_picture_scale_w"] = environment["screen_w"] # how wide to scale the jpg when replaying
     environment["replay_picture_scale_h"] = environment["screen_h"] # how high to scale the jpg when replaying
 
-    #update camera resolution ?!
-    environment["camera_parameters"]["resolution"] = environment["screen_w"], environment["screen_h"]
-
 def unsetup_rpi_camera(environment):
     """ Unset up RPI camera"""
     if environment["camera_pointer"]:
@@ -151,7 +169,7 @@ def setup_rpi_camera(environment):
     environment["camera_pointer"].rotation = environment["camera_parameters"]["rotation"]
     environment["camera_pointer"].hflip = environment["camera_parameters"]["hflip"]
     environment["camera_pointer"].vflip = environment["camera_parameters"]["vflip"]
-    environment["camera_pointer"].brightness = environment["camera_parameters"]["brightness"]
+    #environment["camera_pointer"].brightness = environment["camera_parameters"]["brightness"]
     environment["camera_pointer"].preview_alpha = environment["camera_parameters"]["preview_alpha"]
     environment["camera_pointer"].preview_fullscreen = environment["camera_parameters"]["preview_fullscreen"]
     #environment["camera_pointer"].framerate             = 24
@@ -167,6 +185,7 @@ def setup_rpi_camera(environment):
     #environment["camera_pointer"].image_effect          = 'none'
     #environment["camera_pointer"].color_effects         = None
     #environment["camera_pointer"].crop                  = (0.0, 0.0, 1.0, 1.0)
+    environment["camera_pointer"].video_stabilization = environment["camera_parameters"]["video_stabilization"]
 
 def setup_rpi_gpio(environment):
     """ The RPI GPIO setup"""
@@ -290,7 +309,6 @@ def update_display(environment, BackgroundColor, Message, Numeral, CountDownPhot
     pygame.display.flip()
     return
 
-
 def ShowPicture(environment, file, delay):
     global pygame
     global screenPicture
@@ -306,7 +324,6 @@ def ShowPicture(environment, file, delay):
     ImageShowed = True
     time.sleep(delay)
 
-# display one image on screen
 def show_image(environment, image_path):
     """Display an image on full screen"""
     environment["screen_pointer"].fill(pygame.Color("white")) # clear the screen
@@ -367,7 +384,7 @@ def wait_for_event(environment):
                     return EVENT_TYPE_STOP
                 elif event.key == pygame.K_DOWN:
                     return EVENT_TYPE_TAKE_PICTURE
-        time.sleep(0.2)
+        time.sleep(0.1)
     return EVENT_NO_TYPE
 
 def wait_for_allow_printing_event(environment, seconds_to_wait):
@@ -435,48 +452,63 @@ def take_a_picture(environment, part):
 
     for x in range(3, -1, -1):
         if x == 0:
-            update_display(environment, "black", "PRENEZ LA POSE", "", "", False)
+            update_display(environment, "black", "PRENEZ LA POSE ", "", "", False)
         else:
-            update_display(environment, "black", "PREPAREZ VOUS A PRENDRE LA POSE", "", str(x), False)
+            update_display(environment, "black", "PREPAREZ VOUS A PRENDRE LA POSE ("+str(part)+")", "", str(x), False)
         time.sleep(1)
 
     update_display(environment, "", "", "", "", False)
-    ts = time.time()
-    filename = os.path.join(environment["output_photos_folder"],str(ts)+'-photo.png')
-    environment["camera_pointer"].capture(filename, resize=(IMAGE_WIDTH, IMAGE_HEIGHT))
+    ts = time.strftime('%Y%m%d_%H%M%S', time.localtime())
+    filename_without_extension = os.path.join(environment["output_photos_folder"],str(ts)+'-photo')
+    filename = filename_without_extension+".jpg"
+    #filename = filename_without_extension+".png"
+    environment["camera_pointer"].capture(filename, 'jpeg', use_video_port=True)
     environment["camera_pointer"].stop_preview()
     ShowPicture(environment, filename, 2)
     return filename
 
+def creation_montage(environment, filename1, filename2, filename3):
+    """Creation du montage photo"""
+    image1 = PIL.Image.open(filename1).resize((environment["picture_for_pasting_width"], environment["picture_for_pasting_height"]))
+    image1 = image1.resize((environment["picture_for_pasting_width"], environment["picture_for_pasting_height"]))
+    image2 = PIL.Image.open(filename2).resize((environment["picture_for_pasting_width"], environment["picture_for_pasting_height"]))
+    #image2 = image2.resize((environment["picture_for_pasting_width"], environment["picture_for_pasting_height"]))
+    image3 = PIL.Image.open(filename3).resize((environment["picture_for_pasting_width"], environment["picture_for_pasting_height"]))
+    #image3 = image3.resize((environment["picture_for_pasting_width"], environment["picture_for_pasting_height"]))
+
+    background_image = PIL.Image.open(environment["template_path"])
+    #background_image.paste(image1, (625, 30))
+    #background_image.paste(image2, (625, 410))
+    #background_image.paste(image3, (55, 410))
+
+    background_image.paste(image1, environment["picture_for_pasting_pos1"])
+    background_image.paste(image2, environment["picture_for_pasting_pos2"])
+    background_image.paste(image3, environment["picture_for_pasting_pos3"])
+
+    # Create the final filename
+    ts = time.strftime('%Y%m%d_%H%M%S', time.localtime())
+    montage_filename = os.path.join(environment["output_montages_photos_folder"], str(ts)+"-montage.png")
+    # Save it to the usb drive
+    background_image.save(montage_filename)
+    return montage_filename
+
 def take_pictures(environment):
     """Function that handle the scenario take a picture"""
     lg.info("SCENARIO : Take pictures")
+    update_display(environment, "", "C EST PARTI POUR 3 PHOTOS", "", "", False)
+    time.sleep(1)
     filename1 = take_a_picture(environment, "1/3")
     filename2 = take_a_picture(environment, "2/3")
     filename3 = take_a_picture(environment, "3/3")
 
-    update_display(environment, "Montage en cours ...", "", "", "", False)
+    update_display(environment, "", "Montage en cours ...", "", "", False)
 
-    image1 = PIL.Image.open(filename1)
-    image2 = PIL.Image.open(filename2)
-    image3 = PIL.Image.open(filename3)
 
-    background_image = PIL.Image.open(environment["template_path"])
-    background_image.paste(image1, (625, 30))
-    background_image.paste(image2, (625, 410))
-    background_image.paste(image3, (55, 410))
-    # Create the final filename
-    ts = time.time()
-    Final_Image_Name = os.path.join(environment["output_montages_photos_folder"], str(ts)+"-montage.png")
-    # Save it to the usb drive
-    background_image.save(Final_Image_Name)
-    environment["last_taken_picture_path"] = Final_Image_Name
     # Save a temp file, its faster to print from the pi than usb
-    background_image.save(environment["tmp_photo_print_path"])
-    ShowPicture(environment, environment["tmp_photo_print_path"], 3)
-    background_image2 = background_image.rotate(90)
-    background_image2.save(environment["tmp_photo_print_path"])
+    environment["last_taken_picture_path"] = creation_montage(environment, filename1, filename2, filename3)
 
+    show_image(environment, environment["last_taken_picture_path"])
+    time.sleep(SECONDS_TO_WAIT_TO_SHOW_PICTURE_READY_TO_PRINT)
     printing_asked = wait_for_allow_printing_event(environment, 5)
     lg.info("printing_asked:"+str(printing_asked))
     if printing_asked:
@@ -486,7 +518,12 @@ def show_last_picture(environment):
     """Function that handle the scenario take a picture"""
     lg.info("SCENARIO : Show last picture")
     if environment["last_taken_picture_path"]:
-        ShowPicture(environment, environment["last_taken_picture_path"], SECONDS_TO_WAIT_TO_SHOW_PICTURE_READY_TO_PRINT)
+        if False:
+            ShowPicture(environment, environment["last_taken_picture_path"], SECONDS_TO_WAIT_TO_SHOW_PICTURE_READY_TO_PRINT)
+        else:
+            show_image(environment, environment["last_taken_picture_path"])
+            time.sleep(SECONDS_TO_WAIT_TO_SHOW_PICTURE_READY_TO_PRINT)
+
         #ShowPicture(environment, environment["last_taken_picture_path"], 1)
         printing_asked = wait_for_allow_printing_event(environment, 5)
         lg.info("printing_asked:"+str(printing_asked))
@@ -550,6 +587,7 @@ def my_main(main_args):
     init_folders(environment)
     setup_rpi_gpio(environment)
     setup_rpi_camera(environment)
+    compute_picture_size_and_position(environment)
     main_thread = LaunchThread(main_pygame, environment)
     main_thread.start()
     main_thread.join()
